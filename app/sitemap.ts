254b5bd919af68a588204cddecbc8d4c52638cc9
dankarapetian <91783477+dankarapetian@users.noreply.github.com>
2026-08-31 11:26:36 +0200

    Use canonical non-www URL in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.koblevski-vyna.com/",
+      url: "https://koblevski-vyna.com/",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
